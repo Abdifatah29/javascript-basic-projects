0ba1f140538575c2097b83cc7d8728f2fc0e8371
Abdifatah Hassan <abdi.hassannoor01@gmail.com>
2022-07-08 23:45:28 +0200

    Add hex setup functionality

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,28 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+/*
+    * Generate random hex value
+    * Limit hex values to 6
+    * Add event listener
+    * add generated hex value to body
+    * display hex color inside span
+*/
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener("click", function () {
+    let hexColor = "#";
+    
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[returnRandomHex()]
+    }
+
+    document.body.style.backgroundColor = hexColor
+    color.textContent = hexColor
+})
+
+function returnRandomHex() {
+    return Math.floor(Math.random() * hex.length);
+}
